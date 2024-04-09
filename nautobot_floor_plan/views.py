d5f968c3c2a42e3da6da349761c137ae095e3a1a
Travis Smith <141754521+tsm1th@users.noreply.github.com>
2024-04-09 21:39:46 +0000

    Fixes CI issue

--- nautobot_floor_plan/views.py
+++ nautobot_floor_plan/views.py
@@ -43,7 +43,6 @@ class FloorPlanTileUIViewSet(
     ObjectEditViewMixin,
     ObjectDestroyViewMixin,
     ObjectChangeLogViewMixin,
-    ObjectNotesViewMixin,
     ObjectPermissionRequiredMixin,
 ):  # No need to support bulk operations
     """ViewSet for FloorPlanTile views."""
