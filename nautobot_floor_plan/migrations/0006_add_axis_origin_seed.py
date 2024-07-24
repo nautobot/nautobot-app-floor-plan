b84e9bf94dcf9ebf127f7a04f38d4c748915689e
Ryan Gillespie <24619595+nopg@users.noreply.github.com>
2024-07-24 20:49:27 +0000

    black on the migration file

--- nautobot_floor_plan/migrations/0006_add_axis_origin_seed.py
+++ nautobot_floor_plan/migrations/0006_add_axis_origin_seed.py
@@ -7,28 +7,28 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('nautobot_floor_plan', '0005_add_rackgroup'),
+        ("nautobot_floor_plan", "0005_add_rackgroup"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='floorplan',
-            name='x_origin_seed',
+            model_name="floorplan",
+            name="x_origin_seed",
             field=models.PositiveSmallIntegerField(default=1, validators=[django.core.validators.MinValueValidator(0)]),
         ),
         migrations.AddField(
-            model_name='floorplan',
-            name='y_origin_seed',
+            model_name="floorplan",
+            name="y_origin_seed",
             field=models.PositiveSmallIntegerField(default=1, validators=[django.core.validators.MinValueValidator(0)]),
         ),
         migrations.AlterField(
-            model_name='floorplantile',
-            name='x_origin',
+            model_name="floorplantile",
+            name="x_origin",
             field=models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(0)]),
         ),
         migrations.AlterField(
-            model_name='floorplantile',
-            name='y_origin',
+            model_name="floorplantile",
+            name="y_origin",
             field=models.PositiveSmallIntegerField(validators=[django.core.validators.MinValueValidator(0)]),
         ),
     ]
