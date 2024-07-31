c4d640077625ac75796a0ae544f16a56309a0401
DJ Howard <45186384+djhoward12@users.noreply.github.com>
2024-07-31 16:29:51 -0400

    Update nautobot_floor_plan/signals.py
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- nautobot_floor_plan/signals.py
+++ nautobot_floor_plan/signals.py
@@ -18,8 +18,7 @@ def post_migrate_create__add_statuses(sender, *, apps=global_apps, **kwargs):
     for model_name, default_statuses in PLUGIN_SETTINGS.get("default_statuses", {}).items():
         model = sender.get_model(model_name)
         for status in default_statuses:
-            Status.objects.get_or_create(name=status["name"], defaults={"color": status["color"]})
-            ct_status = Status.objects.get(name=status["name"])
+            ct_status, _ = Status.objects.get_or_create(name=status["name"], defaults={"color": status["color"]})
             ct_model = ContentType.objects.get_for_model(model)
             if ct_model not in ct_status.content_types.all():
                 ct_status.content_types.add(ct_model)
