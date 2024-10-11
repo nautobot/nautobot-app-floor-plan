25695bf65c7f3868772321c0a0f86097b0130c19
Stephen Kiely <stephenkiely87@gmail.com>
2024-10-11 08:45:10 -0500

    Remove invalid views
    These views don't go anywhere as they had invalid url_paths. The model they reference is using NautobotUIViewSet which already adds the ChangeLogView and ObjectNotesView.

--- nautobot_floor_plan/urls.py
+++ nautobot_floor_plan/urls.py
@@ -4,9 +4,8 @@ from django.templatetags.static import static
 from django.urls import path
 from django.views.generic import RedirectView
 from nautobot.apps.urls import NautobotUIViewSetRouter
-from nautobot.core.views import ObjectChangeLogView, ObjectNotesView
 
-from nautobot_floor_plan import models, views
+from nautobot_floor_plan import views
 
 app_name = "floor_plan"
 router = NautobotUIViewSetRouter()
@@ -14,18 +13,6 @@ router.register("floor-plans", views.FloorPlanUIViewSet)
 router.register("floor-plan-tiles", views.FloorPlanTileUIViewSet)
 
 urlpatterns = [
-    path(
-        "models/<uuid:pk>/changelog/",
-        ObjectChangeLogView.as_view(),
-        name="floorplan_changelog",
-        kwargs={"model": models.FloorPlan},
-    ),
-    path(
-        "models/<uuid:pk>/notes/",
-        ObjectNotesView.as_view(),
-        name="floorplan_notes",
-        kwargs={"model": models.FloorPlan},
-    ),
     path("locations/<uuid:pk>/floor_plan/", views.LocationFloorPlanTab.as_view(), name="location_floor_plan_tab"),
     path("docs/", RedirectView.as_view(url=static("nautobot_floor_plan/docs/index.html")), name="docs"),
 ]
