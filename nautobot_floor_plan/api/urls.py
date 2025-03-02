da766ce1d912ccbac36a4c6d1b81d08f10e3d53d
“DJ <dj.howard@networktocode.com>
2025-03-02 09:01:18 -0500

    removing second app_name from urls.

--- nautobot_floor_plan/api/urls.py
+++ nautobot_floor_plan/api/urls.py
@@ -4,7 +4,6 @@ from nautobot.apps.api import OrderedDefaultRouter
 
 from nautobot_floor_plan.api import views
 
-app_name = "nautobot_floor_plan-api"
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
 router.register("floor-plans", views.FloorPlanViewSet)
