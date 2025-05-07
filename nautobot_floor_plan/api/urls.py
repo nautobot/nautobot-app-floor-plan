9f49364a30339b76051ee2ae82874942d0f77da5
Stephen Kiely <stephenkiely87@gmail.com>
2025-05-07 15:04:28 -0500

    Revert change to api/urls.py

--- nautobot_floor_plan/api/urls.py
+++ nautobot_floor_plan/api/urls.py
@@ -7,6 +7,7 @@ from nautobot_floor_plan.api import views
 router = OrderedDefaultRouter()
 # add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
 router.register("floor-plans", views.FloorPlanViewSet)
+router.register("floor-plan-tiles", views.FloorPlanTileViewSet)
 
 app_name = "nautobot_floor_plan-api"
 urlpatterns = router.urls
